b20ccefdda5ebd1b6af3c6b9306cff055517efd6
Matt Koster <mattkoster@gmail.com>
2018-06-06 12:35:45 -0500

    Initial

---
+++ bus.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import xmltodict
+from geopy.distance import vincenty
+
+bus_stop_id = "4350" # Ouside 411
+#bus_stop_id = "4471" # Humboldt and Brady
+
+url_stop = "http://realtime.ridemcts.com/bustime/api/v1/getstops"
+
+querystring = {
+	"key":"t382wyxkgmUYuASFYtVPCRaJK",
+	"format":"json",
+	"stpid":bus_stop_id
+}
+
+headers = {
+	'cache-control': "no-cache",
+	'postman-token': "50682994-3eca-5be9-363e-f974a0fdc646"
+}
+
+response = requests.request("GET", url_stop, headers=headers, params=querystring)
+xml_response = xmltodict.parse(response.text)
+
+lat = xml_response['bustime-response']['stop']['lat']
+lon = xml_response['bustime-response']['stop']['lon']
+bus_stop = (lat, lon)
+
+
+url = "http://realtime.ridemcts.com/bustime/api/v1/getvehicles"
+
+querystring = {
+    "key":"t382wyxkgmUYuASFYtVPCRaJK",
+    "format":"json",
+    "rt":"14"
+}
+
+headers = {
+    'cache-control': "no-cache",
+    'postman-token': "7298722d-4f82-f516-243d-e7f70ccaf3fd"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+parsed_json = json.loads(response.text)
+
+for vehicle in parsed_json['bustime-response']['vehicle']:
+	source_bus = ( vehicle['lat'], vehicle['lon'] )
+	delta_distance = int((vincenty(source_bus, bus_stop).meters))
+	print vehicle['rt'] + " " + vehicle['vid'] + " " + vehicle['des'] + " " + \
+		vehicle['lat'] + " " + vehicle['lon'] + " " + str(delta_distance)

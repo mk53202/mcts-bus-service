495e2943e0f4942f6f26653fb1708357ba538aa2
Matt Koster <mattkoster@gmail.com>
2018-06-09 09:00:36 -0500

    Added bus-time.py

--- bus-time.py
+++ bus-time.py
@@ -4,7 +4,7 @@ import xmltodict
 import os
 
 url_site = "http://realtime.ridemcts.com"
-url_api = "/bustime/api/v1/getroutes"
+url_api = "/bustime/api/v1/gettime"
 url_request = url_site + url_api
 querystring = {
 	"key":os.environ['BUSTIME_API_KEY']
@@ -15,10 +15,10 @@ headers = {
 
 response = requests.request("GET", url_request, headers=headers, params=querystring)
 my_dict = xmltodict.parse(response.text)
-for my_rt in my_dict['bustime-response']['route']:
-	print my_rt['rt'] + " - " + my_rt['rtnm']
+
+print my_dict['bustime-response']['tm']
 
 # JSON export
-# my_json = json.dumps(my_dict['bustime-response']['route'],
-							# sort_keys=True,indent=4, separators=(',', ': '))
-# print my_json
+my_json = json.dumps(my_dict['bustime-response']['tm'],
+							sort_keys=True,indent=4, separators=(',', ': '))
+print my_json
